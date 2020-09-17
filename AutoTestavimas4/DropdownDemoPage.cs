e924f3df04a5adb6e5710cb6c032d85a7dd27527
aiste-m <aiste.mockaitiene@gmail.com>
2020-09-17 23:02:25 +0300

    Baigtas testas
    Namu darbas 4

--- AutoTestavimas4/DropdownDemoPage.cs
+++ AutoTestavimas4/DropdownDemoPage.cs
@@ -1,84 +1,72 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoTestavimas4.Page
 {
     public class DropdownDemoPage : BasePage
     {
-        private const string PageAddress = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
-
-        private SelectElement _dayOfWeekDropdown => new SelectElement(Driver.FindElement(By.Id("select-demo")));
-        private SelectElement _dayOfWeekMultipleDropdown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
-
-        private IWebElement _result => Driver.FindElement(By.ClassName("selected-value"));
+        private const string PageAddress = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";  
+        private SelectElement _stateMultipleDropdown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
         private IWebElement _multipleResult => Driver.FindElement(By.ClassName("getall-selected"));
-        private IWebElement _firstResultButton => Driver.FindElement(By.Id("printMe"));
+        private IWebElement _firstResultButton => Driver.FindElement(By.Id("printMe"));         
         private IWebElement _allResultsButton => Driver.FindElement(By.Id("printAll"));
-
+       
         public DropdownDemoPage(IWebDriver webdriver) : base(webdriver)
         {
             Driver.Url = PageAddress;
         }
-
-        public DropdownDemoPage SelectFromDropDownByText(string text)
-        {
-            _dayOfWeekDropdown.SelectByText(text);
-            return this;
-        }
-
-        public DropdownDemoPage SelectFromDropDownByValue(string value)
-        {
-            _dayOfWeekDropdown.SelectByValue(value);
-            return this;
-        }
-
-        public DropdownDemoPage SelectFromMultipleDropDownByText(string text)
-        {
-            _dayOfWeekMultipleDropdown.SelectByText(text);
-            return this;
-        }
-
-        public DropdownDemoPage SelectFromMultipleDropDownByValue(string value)
-        {
-            _dayOfWeekMultipleDropdown.SelectByValue(value);
-            return this;
-        }
-
         public DropdownDemoPage ClickFirstSelectedButton()
         {
             _firstResultButton.Click();
             return this;
         }
-
+       
         public DropdownDemoPage ClickAllSelectedButton()
-        {
+        {           
             _allResultsButton.Click();
             return this;
         }
 
-        public DropdownDemoPage CheckResultText(string weekDay)
+        public DropdownDemoPage CheckStateResultText(string state)
+        {          
+            string fullText = "First selected option is : " + state;
+            Assert.AreEqual(fullText, _multipleResult.Text, $"Tekstai nesutampa, nerodo {state}");          
+            return this;
+        }   
+       
+        //1) Pažymime 3 valstijas, spaudžiame "First Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo pirmą pažymėtą valstiją.
+
+        public DropdownDemoPage SelectThreeStatesFromMultipleDropDownByText(string state1, string state2, string state3)
         {
-            string fullText = "Day selected :- " + weekDay;
-            Assert.AreEqual(fullText, _result.Text, $"Tekstai nesutampa, nerodo {weekDay}");
-
-
-            Assert.IsTrue(_result.Text.Contains(weekDay), $"Tekstai nesutampa, nerodo {weekDay}");
+            Actions actionBuider = new Actions(Driver);
+            IAction selectThreeItems = actionBuider.KeyDown(Keys.Control).Click();
+            _stateMultipleDropdown.SelectByText(state1);
+            _stateMultipleDropdown.SelectByText(state2);
+            _stateMultipleDropdown.SelectByText(state3);           
+            actionBuider.KeyUp(Keys.Control).Build();
+            selectThreeItems.Perform();
             return this;
         }
 
-        public DropdownDemoPage CheckStateResultText(string state)
+        //2) Pažymime 4 valstijas, spaudžiame "Get All Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo visas užžymėtas valstijas.
+        public DropdownDemoPage SelectFourStatesFromMultipleDropDownByText(string state1, string state2, string state3, string state4)
         {
-            string fullText = "First selected option is : " + state;
-            Assert.AreEqual(fullText, _multipleResult.Text, $"Tekstai nesutampa, nerodo {state}");
-
-
-            Assert.IsTrue(_multipleResult.Text.Contains(state), $"Tekstai nesutampa, nerodo {state}");
+            Actions actionBuider = new Actions(Driver);
+            IAction selectFourItems = actionBuider.KeyDown(Keys.Control).Click();
+            _stateMultipleDropdown.SelectByText(state1);
+            _stateMultipleDropdown.SelectByText(state2);
+            _stateMultipleDropdown.SelectByText(state3);
+            _stateMultipleDropdown.SelectByText(state4);            
+            actionBuider.KeyUp(Keys.Control).Build();
+            selectFourItems.Perform();            
             return this;
         }
     }
--- AutoTestavimas4/DropdownDemoTest..cs
+++ AutoTestavimas4/DropdownDemoTest..cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,38 +21,38 @@ namespace AutoTestavimas4
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
 
             _page = new DropdownDemoPage(driver);
-        }
+        }       
 
-        [TestCase("Friday", TestName = "Testing with Friday")]
-        public static void TestSelectFromDropDownByValue(string dayOfWeek)
+        //1) Pažymime 3 valstijas, spaudžiame "First Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo pirmą pažymėtą valstiją.
+
+        [TestCase("Texas", "Florida", "Ohio", "Texas", TestName = "Testing with 3 states")]
+       
+        public static void TestSelectThreeStatesFromMultipleDropDownByText(string state1, string state2, string state3, string state)
         {
-            _page.SelectFromDropDownByValue(dayOfWeek).CheckResultText(dayOfWeek);
+            state1 = "Texas";
+            state2 = "Florida";
+            state3 = "Ohio";
+            _page.SelectThreeStatesFromMultipleDropDownByText(state1, state2, state3).ClickFirstSelectedButton().CheckStateResultText(state);
         }
 
-        [TestCase("Friday", TestName = "Testing with Friday")]
-        public static void TestSelectFromDropDownByText(string dayOfWeek)
+        //2) Pažymime 4 valstijas, spaudžiame "Get All Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo visas užžymėtas valstijas.
+
+        [TestCase("Texas", "Florida", "Ohio", "New York", "Options selected are : Texas,Florida,Ohio,New York", TestName = "Testing with 4 states")]
+        public static void TestSelectFourStatesFromMultipleDropDownByText(string state1, string state2, string state3, string state4, string state)
         {
-            _page.SelectFromDropDownByText(dayOfWeek).CheckResultText(dayOfWeek);
+            _page.SelectFourStatesFromMultipleDropDownByText(state1, state2, state3, state4);                  
+            _page.ClickAllSelectedButton();
+            _page.CheckStateResultText(state);
         }
-
-        [TestCase("Texas", TestName = "Testing with Texas")]
-        public static void TestSelectFromMultipleDropDownByText(string state)
-        {
-            _page.SelectFromMultipleDropDownByText(state).ClickFirstSelectedButton().CheckStateResultText(state);
-        }
-
-
 
         [OneTimeTearDown]
         public static void TearDown()
         {
             _page.CloseBrowser();
-        }
-
-        //base class check one time tear down
+        }     
 
     }
 }
